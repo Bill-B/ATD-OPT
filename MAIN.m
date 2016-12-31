ce5433a7207f7d8670139616c7a179681483efff
Bill-B <toronto_4@hotmail.com>
2016-12-31 02:13:05 -0500

    optimizer seed with best from last run

--- MAIN.m
+++ MAIN.m
@@ -25,13 +25,14 @@ beq = [];
 nvars = 6;
 TolCon_Data = 1e-6; 
 TolFun_Data = 1e-08;
-% lastrun = load('run1.mat');
+lastrun = load('run1.mat');
 options = gaoptimset;
 options = gaoptimset(options,'TolFun', TolFun_Data);
 % options = gaoptimset(options,'CrossoverFraction', 0.5);
 options = gaoptimset(options,'Display', 'iter');
-% options = gaoptimset(options,'InitialPopulation', lastrun.population);
-options = gaoptimset(options,'PlotFcns', {  @gaplotbestf @gaplotbestindiv @gaplotexpectation @gaplotscorediversity @gaplotstopping });
+
+% options = gaoptimset(options,'InitialPopulation', lastrun.x); %seed
+options = gaoptimset(options,'PlotFcns', { @gaplotbestf @gaplotbestindiv @gaplotexpectation @gaplotscorediversity @gaplotstopping });
 options = gaoptimset(options,'Vectorized', 'off');
 options = gaoptimset(options,'UseParallel', 0 );
 options = gaoptimset(options,'Generations',200,'StallGenLimit', 50);
